2f5dfbfe5acb43f4170b64a6c7d737909ad826c2
OV1designs <oscar@ovaldez.com>
2026-02-03 11:26:03 -0700

    Add proper TypeScript types for Cloudflare Platform environment

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,28 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+// Cloudflare Pages environment variables
+interface CloudflareEnv {
+	ADMIN_PASSWORD: string;
+	ANTHROPIC_API_KEY: string;
+	GOOGLE_SERVICE_ACCOUNT_JSON: string;
+	GOOGLE_SHEET_ID: string;
+	GOOGLE_DRIVE_FOLDER_ID: string;
+}
+
 declare global {
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
-		// interface Platform {}
+		interface Platform {
+			env: CloudflareEnv;
+			context: {
+				waitUntil(promise: Promise<unknown>): void;
+			};
+			caches: CacheStorage & { default: Cache };
+		}
 	}
 }
 
